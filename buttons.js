6885442309a68e7a9dea08b13cbb20da74c9f3de
happiness-mabengi <ndengagrace021@gmail.com>
2025-12-09 23:48:10 -0800

    done

---
+++ buttons.js
@@ -0,0 +1,30 @@
+/*
+Some big block comment
+
+
+*/
+
+// Line comments
+
+// Sends a brief message to validate JS loading
+
+// function toggleDark() {
+//     console.log("toggleDark called");
+//     document.body.classList.toggle("dark-mode")
+//     document.getElementById("instructions").classList.toggle("flipped")
+// }
+
+{/* <script>
+    function toggleDark() {
+    document.classList.toggle('dark-mode')
+}
+</script> */}
+
+
+// make a new function toggleDark
+    // log "toggleDark called"
+function toggleDark() {
+    console.log("toggleDark called");
+    document.main.classList.toggle("dark-mode")
+    document.getElementById("instructions").classList.toggle("flipped")
+}
